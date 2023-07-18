a6f5a3e6ee0fcd62e12fdf3a374b860bf1b3aae3
alexander.eismont <alexander0390@hotmail.com>
2023-07-18 10:57:24 +0200

    init rich model kotlin app

---
+++ rich_domain/src/main/kotlin/com/example/domainobjects/DomainobjectsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.domainobjects
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DomainobjectsApplication
+
+fun main(args: Array<String>) {
+	runApplication<DomainobjectsApplication>(*args)
+}
---
+++ rich_domain/src/test/kotlin/com/example/domainobjects/DomainobjectsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.domainobjects
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DomainobjectsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
