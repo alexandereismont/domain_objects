0265dd6c26f22c40d147a0f674e5aa5a6fd6f053
alexander.eismont <alexander0390@hotmail.com>
2023-10-01 18:50:22 +0200

    shop, product and inventory domains

---
+++ rich_domain/src/main/kotlin/com/example/domainobjects/shop/ShopController.kt
@@ -0,0 +1,26 @@
+package com.example.domainobjects.shop
+
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/shops")
+class ShopController(
+    val shopService: ShopService
+) {
+
+    @GetMapping("/{id}")
+    fun getShop(@PathVariable id: String): String {
+        return "hello shop $id"
+    }
+
+    @PostMapping
+    fun saveShop(@RequestBody shopDto: ShopDto): String {
+        return shopService.saveShop(shopDto)
+    }
+
+    @PostMapping("inventory")
+    fun addProductToInventory(@RequestBody shopProductDto: ShopProductDto): Long? {
+        return shopService.addProduct(productId = shopProductDto.productId, shopId = shopProductDto.shopId)
+    }
+
+}
